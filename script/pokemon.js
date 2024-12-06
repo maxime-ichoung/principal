b3c19fb789ff347eddacbaf30549506f8317d3c6
Daria <dd.chemloul@gmail.com>
2024-12-06 06:02:04 +0100

    Add qrcode generator

---
+++ script/pokemon.js
@@ -0,0 +1,15 @@
+const buttonQRcode = document.getElementById('qrcode-button');
+const popupQRcode = document.getElementById('popup-qrcode');
+
+const pokemonNames = ["bulbizarre", "carapuce", "dinoclier", "dracolosse", "ectoplasma", "ferosinge", "galekid", "hericendre", "magicarpe", "mew", "mysterbe", "noctali", "pichu", "pikachu", "rondoudou", "sabelette", "salameche", "smogo", "togepi"];
+
+buttonQRcode.addEventListener('click', () => {
+    var srcLink = `./assets/${pokemonNames[Math.floor(Math.random() * pokemonNames.length)]}.png`;
+    if (popupQRcode.children.length) {
+        popupQRcode.children[0].src = srcLink;
+    } else {
+        const img = document.createElement("img");
+        img.src = srcLink;
+        popupQRcode.appendChild(img);
+    }
+  });
